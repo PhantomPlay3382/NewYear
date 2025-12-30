2e59c4ab9c54744ba0c9f4d3cd3eee8d918c39df
Phantom_Student <arsenijfonkic18@gmail.com>
2025-12-30 19:15:18 +0200

    1.1 Add class User and Main Add for class User 4 variable type and Constructor

---
+++ src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public User(int id, String firstName, String lastName, double balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+}
