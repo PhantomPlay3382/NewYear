06ec5cf3cbe8e846c6631347336d87994d67a092
Phantom_Student <arsenijfonkic18@gmail.com>
2025-12-30 20:51:27 +0200

    1.4 Add for class Main Method isID Add for class Main optimisation for Method showBalance with method isID

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
 public class Main {
+    private static User[] users = new User[4];
+
     public static void main(String[] args) {
         User u1 = new User(1, "Іван", "Петренко", 1500.50);
         User u2 = new User(2, "Марія", "Коваль", 2800.00);
         User u3 = new User(3, "Олег", "Сидорчук", 450.75);
         User u4 = new User(4, "Анна", "Микитенко", 10200.00);
 
-        User[] users = {u1, u2, u3, u4};
+        users = new User[]{u1, u2, u3, u4};
+        showBalance(1);
+        showBalance(5);
 
-        showBalance(users, 2);
-        showBalance(users, 5);
     }
-
-    public static void showBalance(User[] users, int searchId) {
+    public static User isID(int searchId){
         boolean found = false;
 
         for (User i : users) {
             if (i.id == searchId) {
-                System.out.println("Користувач: " + i.firstName + " " + i.lastName);
-                System.out.println("Сума на рахунку: " + i.balance + " грн.");
-                found = true;
-                break;
+                return i;
             }
         }
-
-        if (!found) {
-            System.out.println("Користувача з ID " + searchId + " не знайдено.");
+        return null;
+    }
+    public static void showBalance(int id) {
+        try {
+            User user = isID(id);
+            System.out.println("Користувач: " + user.firstName + " " + user.lastName);
+            System.out.println("Сума на рахунку: " + user.balance + " грн.");
+        } catch (NullPointerException e) {
+            System.out.println("Такого користувача немає");
         }
     }
 }
