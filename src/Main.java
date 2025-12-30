6e7ea707acdf06fcfec2547c128c23cceee641de
Phantom_Student <arsenijfonkic18@gmail.com>
2025-12-30 19:51:41 +0200

    1.3 Add for class Main Method "showBalance" Its function is to show the balance of a person from the "users" list by ID.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        User u1 = new User(1, "Іван", "Петренко", 1500.50);
+        User u2 = new User(2, "Марія", "Коваль", 2800.00);
+        User u3 = new User(3, "Олег", "Сидорчук", 450.75);
+        User u4 = new User(4, "Анна", "Микитенко", 10200.00);
 
-        User user1 = new User(1, "Арсеній", "Фонкич", 1500.50);
-        User user2 = new User(2, "Катя", "Шевченко", 2700.00);
-        User user3 = new User(3, "Олександр", "Петренко", 300.25);
-        User user4 = new User(4, "Марія", "Коваль", 5000.00);
+        User[] users = {u1, u2, u3, u4};
+
+        showBalance(users, 2);
+        showBalance(users, 5);
+    }
+
+    public static void showBalance(User[] users, int searchId) {
+        boolean found = false;
+
+        for (User i : users) {
+            if (i.id == searchId) {
+                System.out.println("Користувач: " + i.firstName + " " + i.lastName);
+                System.out.println("Сума на рахунку: " + i.balance + " грн.");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Користувача з ID " + searchId + " не знайдено.");
+        }
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,8 +1,8 @@
 public class User {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private double balance;
+    int id;
+    String firstName;
+    String lastName;
+    double balance;
 
     public User(int id, String firstName, String lastName, double balance) {
         this.id = id;
