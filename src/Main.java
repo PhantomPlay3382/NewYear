797823c4832ae2675d8c78ee4123c5a27d022bdc
Phantom_Student <arsenijfonkic18@gmail.com>
2025-12-30 19:17:37 +0200

    1.2 Add for class Main 4 class object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+
+        User user1 = new User(1, "Арсеній", "Фонкич", 1500.50);
+        User user2 = new User(2, "Катя", "Шевченко", 2700.00);
+        User user3 = new User(3, "Олександр", "Петренко", 300.25);
+        User user4 = new User(4, "Марія", "Коваль", 5000.00);
     }
 }
